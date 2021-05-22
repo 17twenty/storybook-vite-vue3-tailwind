9cdbf6e4a3cda80a3556d53d20324a5df2e3c28b
Nick Glynn <n.s.glynn@gmail.com>
2021-05-22 16:21:25 +1000

    I hate Vite config

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,10 @@
 <template>
   <div className="w-full h-screen mx-auto container flex flex-col items-center justify-center">
-      <div className=" flex flex-col bg-blue-400 px-16 py-12 rounded-lg shadow-xl">
-        <div className="text-2xl text-white font-extrabold ">Vue + Tailwind + Storybook</div>
-        <div className="text-lg text-white font-medium">Heaps awesome frontend development!</div>
-      </div>
+    <div className=" flex flex-col bg-blue-400 px-16 py-12 rounded-lg shadow-xl">
+      <div className="text-2xl text-white font-extrabold ">Vue + Tailwind + Storybook</div>
+      <div className="text-lg text-white font-medium">Heaps awesome frontend development!</div>
     </div>
+  </div>
 </template>
 
 <script>
@@ -13,12 +13,9 @@ import '../index.css';
 export default {
   name: 'HelloWorld',
   props: {
-    msg: String
+    msg: String,
   },
   data() {
-    return {
-      count: 0
-    }
-  }
-}
+  },
+};
 </script>
